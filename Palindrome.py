047c464adbd672b7c715a624ac15caee6ac31f50
vinay lakkimsetti <120162249+SaiVinaylakkimsetti@users.noreply.github.com>
2023-08-25 13:57:27 +0530

    n = int(input()) r=0 s=n while(n>0):     rem=n%10     r=(r*10)+rem     n=n//10 if s==r:     print("Palindrome") else:     print("Not Palindrome")

--- Palindrome.py
+++ Palindrome.py
@@ -1,7 +1,12 @@
-def is_palindrome(w):
-    return  w == w[::-1]
-w=input()
-if is_palindrome(w):
-    print("True")
+n = int(input())
+r=0
+s=n
+while(n>0):
+    rem=n%10
+    r=(r*10)+rem
+    n=n//10
+if s==r:
+    print("Palindrome")
 else:
-    print("False")
+    print("Not Palindrome")
+    
