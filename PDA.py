1f10d2f9d8378e5d884bcca4805d938ab15b12fa
vinay lakkimsetti <120162249+SaiVinaylakkimsetti@users.noreply.github.com>
2023-08-26 14:14:19 +0530

    n = int(input()) c = 0 for i in range(1,n):     if n%i==0:         c=c+i if c==n:     print('PERFECT') elif c>n:     print('ABUNDANT') elif c<n:     print('DEFICIENT')

---
+++ PDA.py
@@ -0,0 +1,12 @@
+n = int(input())
+c = 0
+for i in range(1,n):
+    if n%i==0:
+        c=c+i
+if c==n:
+    print('PERFECT')
+elif c>n:
+    print('ABUNDANT')
+elif c<n:
+    print('DEFICIENT')
+    
