ea7f9cffe8650ed06ee1ca21ed81c0d299bb1a10
vinay lakkimsetti <120162249+SaiVinaylakkimsetti@users.noreply.github.com>
2023-11-09 15:03:37 +0530

    #maximum sum of column wise sum of a matrix a,b=map(int,input().split()) c=[] for i in range(0,a):     d=list(map(int,input().split()))     c.append(d) m=0 for i in range(0,b):     s=0     for j in range(0,a):         s+=c[j][i]     if s>m:         m=s print(m)

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.py
@@ -0,0 +1,14 @@
+#maximum sum of column wise sum of a matrix
+a,b=map(int,input().split())
+c=[]
+for i in range(0,a):
+    d=list(map(int,input().split()))
+    c.append(d)
+m=0
+for i in range(0,b):
+    s=0
+    for j in range(0,a):
+        s+=c[j][i]
+    if s>m:
+        m=s
+print(m)
